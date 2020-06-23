a6657a8da06a482da97717963160e4839644a2fb
JonatanOrdonez <jonatan-ordonez@hotmail.com>
2020-06-22 21:40:35 -0500

    Develop docker compose for deploy cockroach, backend and frontend containers

--- cockroach/setup_db.sh
+++ cockroach/setup_db.sh
@@ -6,5 +6,5 @@ SQL="/cockroach/cockroach.sh sql --host cockroach --insecure"
 $SQL -e "CREATE USER IF NOT EXISTS tradmin;"
 $SQL -e "CREATE DATABASE catching;"
 $SQL -e "GRANT ALL ON DATABASE catching TO tradmin;"
-$SQL -d catching -e "CREATE TABLE servers(id SERIAL PRIMARY KEY, url TEXT UNIQUE, servers JSONB, updatedAt INTEGER);"
+$SQL -d catching -e "CREATE TABLE domains(id SERIAL PRIMARY KEY, servers JSONB, endpoints JSONB, url TEXT UNIQUE, sslGrade TEXT, previousSslGrade TEXT, logo TEXT, title TEXT, updatedAt INTEGER, serversChanged BOOLEAN, isDown BOOLEAN);"
 echo Database successfully configured
